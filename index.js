46e82120e435ef609d1ff53fee1933f4ffc8f5b5
AdaDesignNote <adawang.zeroone@gmail.com>
2026-06-07 00:45:42 +0800

    提交第一版

---
+++ index.js
@@ -0,0 +1,11 @@
+const menuBtn = document.querySelector('.btn-menu')
+const navList = document.querySelector('.nav-list')
+
+menuBtn.addEventListener('click', function (){
+    navList.classList.toggle('active')
+    document.body.classList.toggle('menu-open')
+})
+
+
+
+
